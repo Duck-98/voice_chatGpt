5a45f2a509f4fa39bcc5aa119662d65eb8a3f149
duck <godqhr2256@gmail.com>
2023-03-23 16:13:21 +0900

    feat: router 설정

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { GlobalStyle } from "./styles/global-style";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
 root.render(
   <React.StrictMode>
     <GlobalStyle />
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
