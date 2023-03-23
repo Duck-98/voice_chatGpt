fc95969218761e88c51f3bf5432c0c96976e7902
duck <godqhr2256@gmail.com>
2023-03-23 21:54:23 +0900

    feat: 음성 인식 hook

---
+++ src/hooks/useVoice.ts
@@ -0,0 +1,27 @@
+interface Props {
+  message: string;
+  pitch: number;
+}
+
+export const useVoice = () => {
+  const speak = ({ message, pitch = 1 }: Props) => {
+    // ai 답변
+    const voice = window.speechSynthesis;
+    let voices = voice.getVoices();
+    const utterThis = new SpeechSynthesisUtterance(message);
+    utterThis.voice = voices[0];
+    utterThis.pitch = pitch;
+    voice.speak(utterThis);
+  };
+
+  const stopVoice = () => {
+    // 음성 인식 중지
+    const voice = window.speechSynthesis;
+    voice.cancel();
+  };
+
+  return {
+    speak,
+    stopVoice,
+  };
+};
